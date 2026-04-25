0fa70e31174b3ac8397076b593f4309bc904ec81
vi3e3e3w <levutruonggiang.2013@gmail.com>
2026-04-25 11:37:23 +0700

    Update bros.py

--- bros.py
+++ bros.py
@@ -1,8 +1,8 @@
 #THKS TO BRILLIANT
-import random,os, time #PLAY RAMDOM WHO CARE =)))
+import random, time, os #PLAY RAMDOM WHO CARE =)))
 #-----Variable-----
-def clear()
-	os.system("cls" if os.name == "nt" else "clear")
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
 users = {
     "admin": "admin",
@@ -18,15 +18,15 @@ def logintest():
     else:
         print("GET OUT, USERNAME NOT IN LIST")
 def loguptest():
-		nw_username = input("username:")
-		nw_password = input("passoword:")
-		limit = 5
-		if len(nw_password) < limit:
-			print("opps, not strong enough")
-		elif nw_username == nw_password:
-			print("TEST FAIL: USERNAME ≠ PASSWORD")
-		else:
-			print("TEST OK")
+                nw_username = input("username:")
+                nw_password = input("passoword:")
+                limit = 5
+                if len(nw_password) < limit:
+                        print("opps, not strong enough")
+                elif nw_username == nw_password:
+                        print("TEST FAIL: USERNAME ≠ PASSWORD")
+                else:
+                        print("TEST OK")
 def reset():
     reset_code = str(random.randint(1000, 9999))
     print("PASTE THE", reset_code, "IN THAT INPUT BRO")
@@ -52,53 +52,53 @@ def reset():
     else:
         print("TEST FAIL: code doesn't match")
 def info():
-	print("""--BIG UPDATE YIPPY ^^--
-	ADD NOW THE BRAND NEW BOARD TO LOGIN
-	nooooo bolean will not appear in this program
-	Q&A will soon in comment (I don't know github has comment)""")
+        print("""--BIG UPDATE YIPPY ^^--
+        ADD NOW THE BRAND NEW BOARD TO LOGIN
+        nooooo bolean will not appear in this program
+        Q&A will soon in comment (I don't know github has comment)""")
 def myfault():
-	print ("Nothing (SO MUCH CHATGPT!!) ")
+        print ("Nothing (SO MUCH CHATGPT!!) ")
 def some_fun():
-	print("what kind of fun are you today?")
-	awnser = input("=>")
-	if awnser == "9008":
-		print("oh no! my phone is Qualcomm =(")
-	elif awnser == "main":
-		print("ok, but tomorrow is not your main")
+        print("what kind of fun are you today?")
+        awnser = input("=>")
+        if awnser == "9008":
+                print("oh no! my phone is Qualcomm =(")
+        elif awnser == "main":
+                print("ok, but tomorrow is not your main")
 #         main
 print("""$$$$$$$$$$$$$$$$$$$$$$$$$$$
 -------Python Bros---------
 $$$$$$$$$$$$$$$$$$$$$$$$$$$
        choose mode
-	1.Login
-	2.Log up
-	3.Reset passworld
-	4.information
-	5:My fault of programing this
-	6:Some kind of fun (beta)
-	clear:clear all this (remember this 'help')
-	exit() quit...for now
-Update will be soon (1.75)""")
+        1.Login
+        2.Log up
+        3.Reset passworld
+        4.information
+        5:My fault of programing this
+        6:Some kind of fun (beta)
+        clear(): clean up screen (Remember this)
+        exit() quit...for now
+Update will be soon (1.75) public """)
 while True:
-	key = input(">>>")
-	if key == "1":
-		logintest()
-	elif key == "2":
-		loguptest()
-	elif key == "3":
-		reset()
-	elif key == "4":
-		info()
-	elif key == "5":
-		myfault()
-	elif key == "6":
-		some_fun()
-	elif key == "clear"
-		clear()
-	elif key == "exit()":
-		print("bye...for now")	
-		break
-	elif key == "exit":
-	    print("just for real, add some ')' pls")
-	else:
-		print("The code does nothing")
+        key = input(">>>")
+        if key == "1":
+                logintest()
+        elif key == "2":
+                loguptest()
+        elif key == "3":
+                reset()
+        elif key == "4":
+                info()
+        elif key == "5":
+                myfault()
+        elif key == "6":
+                some_fun()
+        elf key == "clear":
+                clear()
+        elif key == "exit()":
+                print("bye...for now")        
+                break
+        elif key == "exit":
+            print("just for real, add some ')' pls")
+        else:
+                print("The code does nothing")
